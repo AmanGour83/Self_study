efb7deea73cd27d567c551010589684166ae6664
Aman Gour <amangour5488@gmail.com>
2025-06-20 11:32:36 +0530

    Add files via upload

---
+++ courses/script.js
@@ -0,0 +1,6 @@
+// function changeVideo(url) {
+//     document.getElementById("videoFrame").src = url;
+// }
+function changeVideo(videoId) {
+    document.getElementById("youtube-video").src = videoId;
+}
